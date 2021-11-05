d48af5c1693da94ee8e88c8747f5f2c14a728dcf
ClementDv <61381874+ClementDv@users.noreply.github.com>
2021-11-05 19:12:12 +0000

    Deploying to github-pages from @ ClementDv/my-personal-website@b827bce455f78a851ca7730ade8043a6cd506f17 🚀

---
+++ _next/static/6ZAiXMNvKUeM_pZw1CW43/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/884-6aec542eaf8ddf1611dc.js","static/css/e15e78630be5ad318312.css","static/chunks/pages/index-576e0b2ec134b0f9e44c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
